3ebfaa055969e07878b3249382bb482e93a93a20
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2023-09-19 01:51:36 -0700

    Create Map.java

---
+++ rds_Copy/Map.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Mp {
+	HashMap<String , Course> m;
+	
+	public Mp() {
+		m = new HashMap<String, Course>();
+		m.put("CSE115", new Course("CSE115", "Introduction to Programming I", "3.0"));
+		m.put("CSE215", new Course("CSE215", "Introduction to Programming II", "3.0"));
+		m.put("CSE173", new Course("CSE173", "Dis", "3.0"));
+	}
+}
