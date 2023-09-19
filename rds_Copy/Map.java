f3c0413c8c8a285e0cdd2bfa1212bb4fb1c571f8
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2023-09-19 23:12:54 +0600

    Update Map.java

--- rds_Copy/Map.java
+++ rds_Copy/Map.java
@@ -9,4 +9,8 @@ public class Mp {
 		m.put("CSE215", new Course("CSE215", "Introduction to Programming II", "3.0"));
 		m.put("CSE173", new Course("CSE173", "Dis", "3.0"));
 	}
+	
+	public Course getKey(String s) {
+		return m.get(s);
+	}
 }
