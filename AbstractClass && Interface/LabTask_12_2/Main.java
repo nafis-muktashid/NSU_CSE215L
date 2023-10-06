84e3a4170cbf4c73594568e7b2fbf50ea62a403d
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2023-10-07 00:00:25 +0600

    Create Main.java

---
+++ AbstractClass && Interface/LabTask_12_2/Main.java
@@ -0,0 +1,10 @@
+package LabTask_12_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BestForCustomer b1 = new BestForCustomer(10, 200, 10);
+		System.out.println("Best For Customer: "+b1.discountedPrice(250));
+	}
+
+}
